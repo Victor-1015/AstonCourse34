3055acd2e1e0c255c6a4a10fb0e41a061972329a
Victor S <93828325+Victor-1015@users.noreply.github.com>
2025-08-09 18:13:29 +0300

    Delete src/main/java/com/astoncourse/lesson7 directory

--- src/main/java/com/astoncourse/lesson7/IntegerComparator.java
+++
@@ -1,7 +0,0 @@
-package com.astoncourse.lesson7;
-
-public class IntegerComparator {
-    public static int compare(int a, int b) {
-        return Integer.compare(a, b);
-    }
-}
--- src/main/java/com/astoncourse/lesson7/TriangleAreaCalculator.java
+++
@@ -1,9 +0,0 @@
-package com.astoncourse.lesson7;
-
-public class TriangleAreaCalculator {
-    public static double calculateArea(int base, int height) {
-        if (base <= 0 || height <= 0)
-            throw new IllegalArgumentException("Base and height must be positive");
-        return 0.5 * base * height;
-    }
-}
